0a9ebd46a68aa23ed602a08765d1ad9f49a04b71
mila-zh <105249447+mila-zh@users.noreply.github.com>
2022-06-13 11:22:47 -0400

    installed mongoose and connected db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('./app');
+var app = require('./configs/app');
 var debug = require('debug')('lyudmilazhurbacomp229sec006-lab1:server');
 var http = require('http');
 
