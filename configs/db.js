a2146a7eaf352075f1243f128b8f30d479bb6ffb
mila-zh <105249447+mila-zh@users.noreply.github.com>
2022-06-25 11:37:34 -0400

    db connection

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/milas_database",
+    "URI": "mongodb+srv://mila:Kazak123@cluster0.dyoff.mongodb.net/milas_database?retryWrites=true&w=majority",
     "Secret": "Secret"
 }
 
